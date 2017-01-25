a02c19b34f7343d487b5896f03b694e1cc27c080
thowell <tamara.howell@hotmail.com>
2017-01-25 15:44:10 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,6 @@
+/* document.getElementById("aboutMe").innerHTML; */
+
+var aboutMe = document.getElementById("aboutMe");
+
+var aboutMeContent = "<h1>About Me</h1>"
+					+ "<p>This is all about me.</p>";
