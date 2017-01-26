2a2736ee79e82a6be1579b1f8c242c6d19cd842d
thowell <tamara.howell@hotmail.com>
2017-01-25 19:22:00 -0500

    bootstrap and jquery installed

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,15 @@
 /* document.getElementById("aboutMe").innerHTML; */
 
+//var aboutMe = document.getElementById("aboutMe");
+
+//var aboutMeContent = "<h1>About Me</h1>"
+					//+ "<p>This is all about me.</p>";
+
+//aboutMe = aboutMeContent;
+
 var aboutMe = document.getElementById("aboutMe");
 
-var aboutMeContent = "<h1>About Me</h1>"
-					+ "<p>This is all about me.</p>";
+var aboutContent = "This is all about me.";
+
+aboutMe.innerContent = aboutContent;
+
