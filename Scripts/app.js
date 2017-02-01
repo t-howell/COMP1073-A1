86f2124a6614b7e749bc507003550a0becb5790d
thowell <tamara.howell@hotmail.com>
2017-01-31 20:49:18 -0500

    Projects page

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,13 +3,12 @@
 // Site: http://comp1073-a1.azurewebsites.net/
 // Description: Contains JavaScript for About Me page
 
-console.log("Page works kinda");
 
 //reference an element
 var aboutMe = document.getElementById("aboutMe");
 
 //create variable that contains content
-var aboutContent = "My name is Tamara. I'm a second semester web design student at Georgian College. "
+var aboutContent = "Hello! My name is Tamara. I'm a second semester web design student at Georgian College. "
 				+ "I enjoy long walks, gardening and knitting. I'm like your Nana, but decent with computers. "
 				+ "My interest in design stems from my enjoyment of helping others. I actively imagine the kinds of "
 				+ "applications and well-built websites that could be useful to people." ;
